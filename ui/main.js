7ef00e185bd1eb4a1b6fd20c1929851e7b739da3
Joshua Dani M <36666984+jd4joshua@users.noreply.github.com>
2018-02-25 18:53:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,12 +20,24 @@ var nameInput=document.getElementById('name');
 var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
+     var request=new XMLHttpRequest();
     
-    var names=['name1','name2','name3',['name4']];
-    var llist='';
-    for(var i=0;i<names.lenght;i++){
-        list+='<li>'+names[i]+'<li>';
-    }
-    var ul=document.getElementById('namelist');
-    ul.innerHTML=list;
+    request.onreadystatechange=function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                var names=request.responseText;
+                names=JSON.parse(names);
+                var llist='';
+                for(var i=0;i<names.lenght;i++){
+                    list+='<li>'+names[i]+'<li>';
+                }
+                var ul=document.getElementById('namelist');
+                ul.innerHTML=list;
+            }
+        }
+        
+    };
+    request.open('GET','http://jd4joshua.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(null);
+    
 };
