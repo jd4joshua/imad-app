dc8bfbdca6a478c06ad7b7626035a997b061f1e9
Joshua Dani M <36666984+jd4joshua@users.noreply.github.com>
2018-02-25 18:05:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,17 @@
-console.log('Loaded!');
-
-var element=document.getElementById('main-text');
-element.innerHTML='New value';
-
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft+"px";
-}
-img.onclick=function(){
-    var interval=setInterval(moveRight,50);
+var button=document.getElementById('counter');
+button.onclick = function() {
+    var request=new XMLHttpRequest();
+    
+    request.onreadystatechange=function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                var counter=request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+        
+    };
+    request.open('GET','http://jd4joshua.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
